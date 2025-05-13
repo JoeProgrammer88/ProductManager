54233c5960b1b87d62f9dd784185325e4b34eb63
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2025-05-13 11:16:12 -0700

    Add UI components to add a product for the main form
    Added a field for the user to enter a product name on the main form

--- ProductManager/Form1.Designer.cs
+++ ProductManager/Form1.Designer.cs
@@ -28,19 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Product Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(149, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(94, 223);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 65);
+            button1.TabIndex = 2;
+            button1.Text = "Add Product";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(325, 402);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Product Manager";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
